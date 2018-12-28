ec30e7b8d5fe8f35c8a601911795afa6bcabe510
SzilvasiPeter <peti.szilvasi95@gmail.com>
2018-12-28 20:51:17 +0100

    Problem solver by adding getter/setter to our entity

--- src/main/java/com/szilvasipeter/springboot/springboot/LoadDatabase.java
+++ src/main/java/com/szilvasipeter/springboot/springboot/LoadDatabase.java
@@ -3,6 +3,7 @@ package com.szilvasipeter.springboot.springboot;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 
 @Configuration
@@ -11,12 +12,12 @@ public class LoadDatabase {
     @Bean
     CommandLineRunner init(ChapterRepository repository){
         return args -> {
-            Flux.just(
-                new Chapter("Quick Start with Java"),
-                new Chapter("Reactive Web with Spring Boot"),
-                new Chapter("... and more"))
-                .flatMap(repository::save)
-                .subscribe(System.out::println);
+            Disposable subscribe = Flux.just(
+                    new Chapter("Quick Start with Java"),
+                    new Chapter("Reactive Web with Spring Boot"),
+                    new Chapter("... and more"))
+                    .flatMap(repository::save)
+                    .subscribe(System.out::println);
         };
     }
 }
